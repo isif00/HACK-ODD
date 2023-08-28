3c45347203c953cfe672180043b99eb1e93cbe39
isif00 <sifodev@gmail.com>
2023-08-29 00:37:03 +0100

    ref: change styling options

--- lib/widgets/text_input.dart
+++ lib/widgets/text_input.dart
@@ -13,15 +13,15 @@ class _TextInputState extends State<TextInput> {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      style: Styles.headLineSmall.copyWith(fontWeight: FontWeight.w500),
+      style: Styles.headLineSmall.copyWith(fontWeight: FontWeight.w800),
       decoration: InputDecoration(
         contentPadding:
-            const EdgeInsets.symmetric(vertical: 25.0, horizontal: 20.0),
+            const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
         focusedBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Styles.titleColor, width: 2.5),
             borderRadius: BorderRadius.circular(8.0)),
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Styles.titleColor, width: 2.0),
+            borderSide: BorderSide(color: Styles.disabledColor, width: 2.0),
             borderRadius: BorderRadius.circular(8.0)),
         hintText: widget.hintText,
       ),
