2bc63671c2b9fa51c055cd5909cce293a8bc3510
Salmi Sifeddine <116845247+isif00@users.noreply.github.com>
2023-09-13 22:12:57 +0100

    Sifo (#32)
    * Feat: Adding Email SignIn Screen
    
    * Feat: Adding UserInfoEmail Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hack_odd/screens/events_screen.dart';
-//import 'package:hack_odd/screens/events_screen.dart';
-import 'package:hack_odd/screens/intro_screen.dart';
-import 'package:hack_odd/screens/intro_screens/intro_1.dart';
-import 'package:hack_odd/screens/intro_screens/intro_2.dart';
 import 'package:hack_odd/screens/user_authentification/email_signin.dart';
-import 'package:hack_odd/screens/user_authentification/user_catrgory_selection.dart';
+import 'package:hack_odd/screens/user_authentification/user_info_signin.dart';
+
 import 'package:hack_odd/utils/app_layout.dart';
 import 'package:hack_odd/utils/app_style.dart';
 
@@ -39,7 +35,8 @@ class MyApp extends StatelessWidget {
               borderRadius: BorderRadius.circular(8.0)),
         ),
       ),
-      home: const EmailSignIn(),
+      home: const UserInfoSignIn(),
+
     );
   }
 }
